ef7d2f8aae8e80820471b31d2f5bce60ce849741
sonny parsonage <sonnyparsonage@gmail.com>
2017-05-12 17:33:50 +0100

    adding react-router tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,56 @@ class App extends Component {
       <Router history={browserHistory}>
         <Route path='/' component={Container} >
           <IndexRoute component={Home}/>
-          <Route path='/address' component={Address} />
+            <Route path='/address' component={Address} >
+              <IndexRoute component={twitter_feed}/>
+              <Route path='instagram' component={instagram}/>
+              <Route path='query' component={Query}/>
+            </Route>
+            <Route path='/NamedComponent' component={NamedComponents}>
+              <IndexRoute components={{title:Title2, subTitle:subTitle}}/> 
+            </Route>
+            <Route path='/about/(:name)' component={About}/>
           <Route path='*' component={Notfound} />
         </Route>
       </Router>)
   }
 }
 
-const Home = () => <h1>Hello from Home!</h1>
-const Address = () => <h1>We are located at 555 Jackson St.</h1>
+const Query = (props) => (
+  <h2>{props.location.query.message}</h2>
+  )
 
+const About = (props) => (
+  <div>
+    <h3>Welcome to the about page</h3>
+    { props.params.name && <h2>Hello, {props.params.name}</h2>}
+  </div>)
+
+const NamedComponents = (props) => (
+  <div>
+    {props.title} <br/>
+    {props.subTitle}
+  </div>)
+
+const Title2 = () => (
+  <h1>Hello from title component</h1>
+  )
+
+const subTitle = () => (
+  <h1>Hello from subtitle component</h1>)
+
+const Home = () => <h1>Hello from Home!</h1>
+const Address = (props) => 
+  <div>
+    <br />
+    <Link to='/address'>Twitter Feed</Link>
+    <Link to='/address/instagram'> Instagram feed</Link>
+    <h1>We are still located in the US</h1>
+    {props.children}
+  </div>
+
+const instagram = () => <h3> Instagram feed </h3>
+const twitter_feed = () => <h3> twitter feed </h3>
 
 const Notfound = () => <h1>Page not found....</h1>
 
@@ -27,6 +67,11 @@ const Nav = () => (
   <div>
     <Link to='/'>Home </Link>&nbsp;
     <Link to='/address'>Address </Link>
+    <Link to='/NamedComponent'>Named components</Link>
+    <Link to={{
+                pathname: '/address/query',
+                query: {message: 'Hello from route query'} 
+              }}>Route query</Link>
   </div>
   )
 
