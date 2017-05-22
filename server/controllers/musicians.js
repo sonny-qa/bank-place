e1a5c89d79df4bf7ca45bba3d89705900d6d72fe
sonny parsonage <sonnyparsonage@gmail.com>
2017-05-22 17:25:44 +0100

    fixing deploy

--- server/controllers/musicians.js
+++ server/controllers/musicians.js
@@ -4,32 +4,36 @@ var knex        = require('knex')(config[env]);
 var Mailgun = require('mailgun').Mailgun;
 var mg = new Mailgun('dkey-26f26c48d6f0aa5b7766890e8d9f8d92');
 
-
-
+var client = require('../server.js')
 
 exports.add = function(req,res) {
 	console.log('data',req.body)
 	var test = {First_Name:(req.body.First_Name),Last_Name: (req.body.Last_Name), email: (req.body.Last_Name), apt_num: (req.body.ApartmentNumber), address: (req.body.ContactAddress) }
 	//var test = "(type, color, location, install_date) VALUES ('slide', 'blue', 'south', '2014-04-28')"
-	
-	knex.insert(test).into('users').then(function(id){
-		console.log('ok')
-		return test
-	}).then(function(test){
-	//res.send(200)
-	// mg.sendText('example@example.com', ['Recipient 1 <sonny.abc@gmail.com>'],
- //  	'This is the subject',
- //  	JSON.stringify(test),
- //  	'noreply@example.com', {},
+		client.query('SELECT * FROM users')
+	client.on("end", function (result) {
+    console.log(JSON.stringify(result.rows, null, "    "));
+    client.end();
+	});
 
- //  	function(err) {
- //    if (err) console.log('Oh noes: ' + err);
- //    else     {console.log('Success'); };
-	// 	});
+	// knex.insert(test).into('users').then(function(id){
+	// 	console.log('ok')
+	// 	return test
+	// }).then(function(test){
+	// //res.send(200)
+	// // mg.sendText('example@example.com', ['Recipient 1 <sonny.abc@gmail.com>'],
+ // //  	'This is the subject',
+ // //  	JSON.stringify(test),
+ // //  	'noreply@example.com', {},
 
-	}).finally(function() {
-		res.send(200)
+ // //  	function(err) {
+ // //    if (err) console.log('Oh noes: ' + err);
+ // //    else     {console.log('Success'); };
+	// // 	});
+
+	// }).finally(function() {
+	// 	res.send(200)
   
-});
+//});
 	
 };
--- server/server.js
+++ server/server.js
@@ -7,6 +7,25 @@ var port =4000
 
 const environment = process.env.NODE_ENV || 'development';
 
+
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://goytsjhxznendk:04597c9ae007888488cf04d15ef050093d87065aad4facf2df2d50f792aa0137@ec2-23-23-227-188.compute-1.amazonaws.com:5432/db2iu3epi1gq3c?ssl=true';
+
+
+pg.defaults.ssl = true;
+pg.connect(connectionString, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('CREATE TABLE IF NOT EXISTS users (ID SERIAL PRIMARY KEY, First_Name VARCHAR, Last_Name VARCHAR, email VARCHAR, apt_num INTEGER, address VARCHAR(100))')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
@@ -17,4 +36,5 @@ require('./routes')(app);
 
 app.listen(3001);
 console.log("Jammin\' on port 3001...");
-
+var client = new pg.Client();
+module.exports = client
